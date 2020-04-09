e02fe49761360a5e76c1713e06b2bb377d5d0765
Vinod Chandra <vchandra@macbook.local>
2020-04-08 23:32:04 -0400

    styling

--- src/Component/Accounts/Show/index.js
+++ src/Component/Accounts/Show/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import './index.css';
 import moment from 'moment';
 
 const Edit = (props) => {
--- src/Component/Dashboard/Rollover/index.js
+++ src/Component/Dashboard/Rollover/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './index.css';
 import moment from 'moment'
 import getTransactions from './Transactions'
 
